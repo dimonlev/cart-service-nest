78d67172c4a15855e09db7011f8b675089edd72b
Dmitry Levkovich <dmitry_levkovich@epam.com>
2022-10-30 01:26:18 +0400

    init: add yml file with lambda

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Request, Post, UseGuards, HttpStatus } from '@nestjs/common';
-import { LocalAuthGuard, AuthService, JwtAuthGuard, BasicAuthGuard } from './auth';
+import {
+  Controller,
+  Get,
+  Request,
+  Post,
+  UseGuards,
+  HttpStatus,
+} from '@nestjs/common';
+import {
+  LocalAuthGuard,
+  AuthService,
+  JwtAuthGuard,
+  BasicAuthGuard,
+} from './auth';
 
 @Controller()
 export class AppController {
-
   constructor(private authService: AuthService) {}
 
-  @Get([ '', 'ping' ])
+  @Get(['', 'ping'])
   healthCheck(): any {
     return {
       statusCode: HttpStatus.OK,
@@ -14,12 +25,15 @@ export class AppController {
     };
   }
 
-  @UseGuards(LocalAuthGuard)
+  // @UseGuards(LocalAuthGuard)
   @Post('api/auth/login')
   async login(@Request() req) {
-    const token = this.authService.login(req.user, 'basic');
+    console.log('req: ', req);
+    console.log('req.user: ', req.body.user);
+    const token = this.authService.login(req.body.user, 'basic');
+    console.log('token: ', token);
 
-    return  {
+    return {
       statusCode: HttpStatus.OK,
       message: 'OK',
       data: {
@@ -28,14 +42,15 @@ export class AppController {
     };
   }
 
-  @UseGuards(BasicAuthGuard)
+  // @UseGuards(BasicAuthGuard)
   @Get('api/profile')
   async getProfile(@Request() req) {
+    console.log('req: ', req);
     return {
       statusCode: HttpStatus.OK,
       message: 'OK',
       data: {
-        user: req.user,
+        user: req.body.user,
       },
     };
   }
